bd810e521e71571c1032e94a55d393b250a37f7a
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-09-12 16:27:30 +0200

    icons

--- src/Components/PageHeader.jsx
+++ src/Components/PageHeader.jsx
@@ -1,12 +1,28 @@
+import plus from '../assets/plus.svg'
+import pencil from '../assets/pencil.svg'
+import trash from '../assets/trash.svg'
+import { Link } from 'react-router-dom'
 export default function (props){
     return(
-        <header>
+        <header id="page-header">
             <h2>
                 {props.super}
             </h2>
             <h1>
                 {props.heading}
             </h1>
+            <span id="icon-container">
+            <Link to={`${props.url}/create`} >
+            <img src={plus}/>    
+            </Link>
+            <Link to={`${props.url}${props?.id?`/${props.id}`:""}/edit`} >
+            <img src={pencil}/>
+            </Link>
+            <Link to={`${props.url}${props?.id?`/${props.id}`:""}/delete`} >
+            <img src={trash}/>
+            </Link>  
+            </span>
+            
         </header>
     )
 }
